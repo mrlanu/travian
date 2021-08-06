ee505eb6c7552583e66cc97a1a024ca4ae19a289
mrlanu <mrlanu@gmail.com>
2021-08-05 19:14:24 -0500

    init commit

---
+++ src/main/java/io/lanu/travian/TravianApplication.java
@@ -0,0 +1,13 @@
+package io.lanu.travian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lanu/travian/TravianApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.travian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
